8bbfcad35cbbb3993d1138aff1af90365bcf5c64
Mitchell Blom <mitchellblom@gmail.com>
2017-01-23 21:35:56 -0600

    end of class and on orphan step

---
+++ main.js
@@ -0,0 +1,6 @@
+var phrase = "Hey, look at me!";
+
+var element = document.getElementById("container");
+console.log(element);
+
+element.innerHTML = "<b>hello!</b>";
---
+++ sonnet.js
@@ -0,0 +1,17 @@
+// Paste the code below into your HTML file.
+// Take the contents of the sonnet div and place it in a variable
+
+var element = document.getElementById("sonnet")
+var sonnet = element.innerHTML;
+console.log("sonnet", sonnet);
+
+// Find and output the starting position of the word "orphans"
+
+
+
+// Output the number of characters in the sonnet
+// Replace the first occurance of the string "winter" with "yuletide"
+// Replace all occurances of the string "the" with "a large"
+// Set the content of the sonnet div with the new string
+
+
