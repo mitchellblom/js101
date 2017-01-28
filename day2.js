68252e9e0f5955ebe539a17cea80579d905406cf
Mitchell Blom <mitchellblom@gmail.com>
2017-01-28 10:43:42 -0600

    changes from class

--- day2.js
+++ day2.js
@@ -1,98 +1,103 @@
-// // //switch statement
+// //switch statement
 
-// // var day = "Friday";
+// var day = "Friday";
 
-// // switch(day) {
-// // 	case "Monday":
-// // 		console.log("Booooo");
-// // 		break;
-// // 	case "Friday":
-// // 		console.log("PARTAYYY");
-// // 		break;
-// // 	default: 
-// // 		console.log("not Monday")
-// // }
-
-// // if(day === "Monday") {
-// // 	console.log("Booooo");
-// // } else if(day === "Friday"){
-// // 	console.log("PARTAYYY");
-// // } else {
-// // 	console.log("not Monday")
-// // }
-
-
-// // ARRAYS
-
-// // var fruit = ["cherries", "apples", "bananas"];
-
-// // var fruitString = fruit.join(" cats ")
-
-// // console.log("fruitString", fruitString);
-
-// // var catColors = 'green,red,yellow,black';
-// // var catColorsArray = catColors.split(",");
-// // console.log("catColorsArray", catColorsArray);
-
-// // var reverseFruit = fruit.reverse();
-// // console.log("reverseFruit", reverseFruit)
-
-// // var alphaFruit = fruit.sort();
-// // console.log("alphaFruit", alphaFruit);
-
-// // var nums = [9,6,8,2,4,11,66]; // 2,4,6,8,9,11,66 is what i want
-// // var numSort = nums.sort(function(a,b){return a-b});
-// // console.log("numSort", numSort);
-
-// // var mixedCrap = ["r", "m", 33, 9];
-// // console.log("???", mixedCrap.sort());
-
-// // var animals = ["cats", "turtles", "fish", "bears", "tigers"];
-// // console.log("fish index", animals.indexOf("fish"));
-
-// // var animals2 = animals.slice(2, 4);
-// // console.log("animals2", animals2);
-
-// // animals.splice(1,1);
-// // console.log("noturtles", animals);
-
-// // var colors = ["red", "blue", "gold", "pink", "orange"];
-
-// // //sort, reverse, make a string
-// // var finalAnswer = colors.sort().reverse().join(" cat ");
-// // console.log("finalAnswer", finalAnswer);
-
-// // push
-// // pop
-// // shift
-// // unshift
-
-// for (var i=0; i<5; i++) {
-// 	console.log("i", i);
+// switch(day) {
+// 	case "Monday":
+// 		console.log("Booooo");
+// 		break;
+// 	case "Friday":
+// 		console.log("PARTAYYY");
+// 		break;
+// 	default: 
+// 		console.log("not Monday")
 // }
 
-// // var fruit = ["cherries", "apples", "bananas"];
-
-// // for(var j=0; j<fruit.length; j++) {
-// // 	console.log("fruit", fruit[j]);
-// // }
-
-// // write a for loop that increments by 10 each time = 10 times... 10,20,30,40,50 to 100
-
-// 	for(var k=0; k<101; k+=10) {
-// 		console.log("multiples of 10", i);
-// 	}
-
-// // for loop that pushes the index to an array for 5 times console.log(array) \\ [0,1,2,3,4]
-
-// 	var array2 = [1,2,3,4,5,5,6,7,8,9];
-// 	for(var q=0; q<5; q+=1) {
-// 		console.log("q", q);
-// 	}
+// if(day === "Monday") {
+// 	console.log("Booooo");
+// } else if(day === "Friday"){
+// 	console.log("PARTAYYY");
+// } else {
+// 	console.log("not Monday")
+// }
 
 
-// // for loop that starts at 100 and divides by 2 each time 3 times... 100, 50, 25
+// ARRAYS
 
+// var fruit = ["cherries", "apples", "bananas"];
+
+// var fruitString = fruit.join(" cats ")
+
+// console.log("fruitString", fruitString);
+
+// var catColors = 'green,red,yellow,black';
+// var catColorsArray = catColors.split(",");
+// console.log("catColorsArray", catColorsArray);
+
+// var reverseFruit = fruit.reverse();
+// console.log("reverseFruit", reverseFruit)
+
+// var alphaFruit = fruit.sort();
+// console.log("alphaFruit", alphaFruit);
+
+// var nums = [9,6,8,2,4,11,66]; // 2,4,6,8,9,11,66 is what i want
+// var numSort = nums.sort(function(a,b){return a-b});
+// console.log("numSort", numSort);
+
+// var mixedCrap = ["r", "m", 33, 9];
+// console.log("???", mixedCrap.sort());
+
+// var animals = ["cats", "turtles", "fish", "bears", "tigers"];
+// console.log("fish index", animals.indexOf("fish"));
+
+// var animals2 = animals.slice(2, 4);
+// console.log("animals2", animals2);
+
+// animals.splice(1,1);
+// console.log("noturtles", animals);
+
+// var colors = ["red", "blue", "gold", "pink", "orange"];
+
+// //sort, reverse, make a string
+// var finalAnswer = colors.sort().reverse().join(" cat ");
+// console.log("finalAnswer", finalAnswer);
+
+// push
+// pop
+// shift
+// unshift
+
+for (var i=0; i<5; i++) {
+	console.log("i", i);
+}
+
+// var fruit = ["cherries", "apples", "bananas"];
+
+// for(var j=0; j<fruit.length; j++) {
+// 	console.log("fruit", fruit[j]);
+// }
+
+// write a for loop that increments by 10 each time = 10 times... 10,20,30,40,50 to 100
+
+	for(var k=0; k<101; k+=10) {
+		console.log("multiples of 10", i);
+	}
+
+// for loop that pushes the index to an array for 5 times console.log(array) \\ [0,1,2,3,4]
+
+	var array2 = [1,2,3,4,5,5,6,7,8,9];
+	for(var q=0; q<5; q+=1) {
+		console.log("q", q);
+	}
+
+
+// for loop that starts at 100 and divides by 2 each time 3 times... 100, 50, 25
+
+var num = 100
+for (var j=0; j<3; j++) {
+	console.log("Number: ", num);
+	num = num/2;
+}
 
 
 
